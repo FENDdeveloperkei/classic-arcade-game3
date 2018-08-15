9d07dc3515b36bf8ad45b5255cf7a8c3270b85ed
Lakeshia Williams <38958988+FENDdeveloperkei@users.noreply.github.com>
2018-08-14 19:25:26 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,90 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Creaties enemy class and allows for them to be drawn on screen. Enemies are created later on in this file using the function below.
+const Enemy = function(x,y,speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//Has enemy move across screen and once off screen restart from the other side of the screen. The -100 means that the enemy will come in from off screen.
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+   if (this.x < 495) {
+     this.x += this.speed * dt;
+   } else {
+     this.x = -100;
+   }
 };
-
-// Draw the enemy on the screen, required method for game
+//Draws enemy based on specified x and y.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Creates player class and specifies the pink girl to start at the middle/bottom of screen and draws player on the screen
+class Player {
+  constructor () {
+    this.x = 205;
+    this.y = 405;
+    this.sprite = 'images/char-pink-girl.png';
+  }
+  //Draws girl on screen based on place
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  //Moves player based on the arrow keystroke input, moves one box per key.
+  handleInput(direction){
+    switch(direction) {
+      case 'up' :
+        if (this.y > -11) {
+          this.y -= 83;
+        }
+        break;
+      case 'down' :
+        if (this.y < 405) {
+          this.y += 83;
+        }
+        break;
+      case 'left' :
+        if (this.x > 50) {
+          this.x -= 100;
+        }
+        break;
+      case 'right' :
+        if (this.x < 405) {
+          this.x += 100;
+        }
+        break;
+    }
+  }
+  //Function for resetting the game, called when collision occurs or game is won.
+  reset() {
+    this.y = 405;
+    this.x = 205;
+  }
+  //Update checks for collisions and checks if the game is won. If collision happens then the game is reset. If player wins, an alert is shown and game is reset.
+  update(){
+    for (let enemy of allEnemies) {
+      //This code sourced from Matthew Cranford project walkthrough and adapted for the rest of the project code. Code test for collision and resets the game if there is a collision.
+      if (this.y === enemy.y && (enemy.x + 50 > this.x && enemy.x -50 < this.x)) {
+        this.reset();
+      }
+    }
+    if (this.y === -10) {
+      this.y -= 10;
+      alert('You win!');
+      this.reset();
+    }
+  }
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Creates player and enemies and places them at different places on the board
+const enemy1 = new Enemy(05, 73, 150);
+const enemy2 = new Enemy(105, 156, 50);
+const enemy3 = new Enemy(305, 156, 200);
+const enemy4 = new Enemy(05, 239, 100);
+const enemy5 = new Enemy(405, 239, 200);
+const allEnemies =[enemy1, enemy2, enemy3, enemy4, enemy5];
+const player = new Player();
 
 
 
