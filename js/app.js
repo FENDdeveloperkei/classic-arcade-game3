3328894c629d05132b68e43d361fc6fde7b39fc7
Lakeshia Williams <38958988+FENDdeveloperkei@users.noreply.github.com>
2018-08-20 14:54:56 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,140 +1,114 @@
-/**
- * TODO:
- * - Add score
- * - Add player lives
- * - Add Gem class
- * - Add gameOver screen
- * - Media query
- */
-
-// Enemies the player must avoid
-var Enemy = function(x, y, speed) {
-   
+// Enemies our player must avoid
+class Enemy {
+  constructor(x,y,speed){
+    this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = speed;
-
-    
-    this.sprite = 'https://raw.githubusercontent.com/udacity/frontend-nanodegree-arcade-game/master/images/enemy-bug.png';
-};
-
-
-Enemy.prototype.update = function(dt) {
-    
+    this.speed =1;
+  }
+  // Draw the enemy on the screen, required method for game
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 75);
+  }
+  update(dt){
     this.x += this.speed * dt;
+  }
+}
+const enemy = new Enemy();
 
-    
-    if (this.x > 300) {
-        this.x = -50;
-        this.speed = 50 + Math.floor(Math.random() * 300);
-    }
+// enemy.update = this.x += this.speed * dt;
 
-    // below code will check for any collisions between player and enemy
-    if (player.x < this.x + 60 &&
-        player.x + 35 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
-        player.x = 200; // re-aligns position.1
-        player.y = 300; // re-aligns position.2
-        score = 0; // resets score back to 0 if player collides with enemy
-        document.getElementById('lvlScore').innerHTML = score; // 
-    }
-};
+class EnemyOne extends Enemy {
+  constructor() {
+    super();
+    this.x = 0;
+    this.y = 3;
+  }
+  // Draw the enemy on the screen, required method for game
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 75);
+  }
+  update(dt){
+    this.x += this.speed * dt;
+  }
+}
+const enemyOne = new EnemyOne();
+
+class EnemyTwo extends Enemy {
+  constructor() {
+    super();
+    this.x = 0;
+    this.y = 2;
+  }
+  // Draw the enemy on the screen, required method for game
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 75);
+  }
+}
+const enemyTwo = new EnemyTwo();
+
+
+class EnemyThree extends Enemy {
+  constructor() {
+    super();
+    this.x = 0;
+    this.y = 1;
+  }
+  // Draw the enemy on the screen, required method for game
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 75);
+  }
+}
+const enemyThree = new EnemyThree();
+
+// creating the allEnemies array
+const allEnemies = [];
+allEnemies.push(enemyOne);
+allEnemies.push(enemyTwo);
+allEnemies.push(enemyThree);
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+// };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'https://raw.githubusercontent.com/udacity/frontend-nanodegree-arcade-game/master/images/char-princess-girl.png';
-};
+class Player {
+  constructor(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 2;
+    this.y = 5;
+  }
+  // Draw the player on the screen, required method for game
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+  }
+  // Restrict Player from going off the canvas
+  update(){
+    this.x >= 4;
+    this.x <= 0;
+  }
+}
+const player = new Player();
 
-Player.prototype.update = function() {
-    // Below code will stop the player from moving off canvas
-    if (this.y > 380) {
-        this.y = 380;
-    }
 
-    if (this.x > 300) {
-        this.x = -15;
-      this.speed = 15 +
-        Math.floor(Math.random() * 15);
-    }
 
-    if (this.x < 0) {
-        this.x = 0;
-    }
 
-    // Once player reaches top of canvas/water. 1 point will be added to their lvl/score
-    if (this.y < 0) {
-        this.x = 200;
-        this.y = 380;
-        score++;
-        document.getElementById('lvlScore').innerHTML = score;
-        if(score >= 1) {
-            alert(" You did it!");
-            document.getElementById("lvlScore").innerHTML = "0";
-        }
-   
-    } 
-};
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-// Maneuver around the board using standard arrow keys OR WASD keys
-Player.prototype.handleInput = function(keyPress) {
-    switch (keyPress) {
-        case 'left':
-            this.x -= this.speed + 50;
-            break;
-        case 'up':
-            this.y -= this.speed + 30;
-            break;
-        case 'right':
-            this.x += this.speed + 50;
-            break;
-        case 'down':
-            this.y += this.speed + 30;
-            break;
-        case 'a':
-            this.x -= this.speed + 50;
-            break;
-        case 'w':
-            this.y -= this.speed + 30;
-            break;
-        case 'd':
-            this.x += this.speed + 50;
-            break;
-        case 's':
-            this.y += this.speed + 30;
-            break;    
-    }
-};
-
-// Now instantiate your objects.
+// Now instantiate your objects. This is where we need to initiate our enemies and player
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [];
 
-// Position enemies 
-var enemyPosition = [50, 130, 220];
-var player = new Player(200, 400, 50);
-var enemy;
-
-enemyPosition.forEach(function(posY) {
-    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 150));
-    allEnemies.push(enemy);
-});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -143,8 +117,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down',
+        40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    // player.handleInput(allowedKeys[e.keyCode]);
 });
